56e402c57045531969b01759ed2be597e830d17f
Matthew Kong <www.imksc@live.com>
2022-06-03 02:19:36 +0800

    Add project files.

---
+++ PriceWatchEntrySystem/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PriceWatchEntrySystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult DataEntry()
+        {
+            return View();
+        }
+
+        public ActionResult DataVisualization()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public bool IsUserLoggedIn()
+        {
+            string str1 = Request.Cookies["CurUsername"].Value;
+
+            if (str1 != null)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ PriceWatchEntrySystem/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PriceWatchEntrySystem.Models
+{
+    public class HomeModel
+    {
+       
+
+    }
+}
