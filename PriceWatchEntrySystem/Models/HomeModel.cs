13fbc91586f7bffbaab3b46f92e9c212b5194213
Matthew Kong <www.imksc@live.com>
2022-06-03 16:29:11 +0800

    Data Entry

--- PriceWatchEntrySystem/Models/HomeModel.cs
+++ PriceWatchEntrySystem/Models/HomeModel.cs
@@ -24,5 +24,25 @@ namespace PriceWatchEntrySystem.Models
         public string SelectedRole { get; set; }
         public string SelectedSupermarket { get; set; }
     }
+
+    public class PricesTableRows
+    {
+        public string Barcode { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Wellcome_Price { get; set; }
+        public string PARNnShop_Price { get; set; }
+        public string MarketPlace_Price { get; set;  }
+        public string Watsons_Price { get; set; }
+        public string Mannings_Price { get; set; }
+        public string AEON_PRice { get; set; }
+        public string DCHFoodMart_Price { get; set; }
+    }
+
+    public class PriceTableViewModel
+    {
+        public string CatagoryName { get; set; }
+        public List<PricesTableRows> listPricesTableRows { get; set; }
+    }
     
 }
